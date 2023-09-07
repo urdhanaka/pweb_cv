e48e3e23533766c709d05165adf118809ab4ac18
Urdhanaka Aptanagi <46347836+urdhanaka@users.noreply.github.com>
2023-09-07 21:10:46 +0700

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,14 @@
+function scrollToAbout() {
+    var here = document.getElementById('introduction');
+    here.scrollIntoView({ behavior: 'smooth' }, true)
+}
+
+function scrollToSkills() {
+    var here = document.getElementById('skills');
+    here.scrollIntoView({ behavior: 'smooth' }, true)
+}
+
+function scrollFunctionTentangKami() {
+    var here = document.getElementById('scroll-to-here');
+    here.scrollIntoView({ behavior: 'smooth' }, true);
+}
